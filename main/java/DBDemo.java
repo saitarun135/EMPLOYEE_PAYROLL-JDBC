cc227b028346214a8a617a520d669abaf26c7ddf
saitarun135 <saitarun135@gm.com>
2021-04-12 11:50:17 +0530

    [sai]Added: retriving data

--- main/java/DBDemo.java
+++ main/java/DBDemo.java
@@ -4,15 +4,17 @@ import java.sql.DriverManager;
 import java.util.Enumeration;
 
 public class DBDemo {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) {
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service1";
         String userName = "root";
         String password = "SAItarun*1";
         Connection connection;
-
-            Class.forName("com.mysql.jdbc.Driver");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded!");
-
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Can not find drive in the classpath!", e);
+        }
         listDrivers();
         try{
             System.out.println("Connecting to database:" +jdbcURL);
---
+++ test/java/EmployeePayrollServiceTest.java
@@ -0,0 +1,20 @@
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class EmployeePayrollServiceTest {
+	   EmployeePayrollService employeePayrollService;
+	   List<EmployeePayrollData> employeePayrollList;
+
+	  @Test
+	  public void given3EmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount(){
+		  employeePayrollService = new EmployeePayrollService();
+		  employeePayrollList = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+		  Assert.assertEquals(3, employeePayrollList.size());
+		    }
+
+		}
