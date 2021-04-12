47669bb1ede6f5c122eb258770dc4106b30bc339
saitarun135 <saitarun135@gm.com>
2021-04-12 10:46:50 +0530

    [sai]Added: creating database and listing drivers

---
+++ main/java/DBDemo.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DBDemo {
+    public static void main(String[] args) throws ClassNotFoundException {
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service1";
+        String userName = "root";
+        String password = "SAItarun*1";
+        Connection connection;
+
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded!");
+
+        listDrivers();
+        try{
+            System.out.println("Connecting to database:" +jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection is successful!!!!"+connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()){
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println(" "+driverClass.getClass().getName());
+        }
+    }
+}
--- main/java/Database.java
+++
@@ -1,5 +0,0 @@
-public class Database {
-    public static void main(String[] args) {
-        System.out.println("welcome to the JDBC-databce connections");
-    }
-}
