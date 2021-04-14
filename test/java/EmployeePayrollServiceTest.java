ac2756141bcb5512a0c3fca4557489270237ea43
saitarun135 <saitarun135@gm.com>
2021-04-14 11:16:07 +0530

    [sai]Added:retrive data by using date

--- test/java/EmployeePayrollServiceTest.java
+++ test/java/EmployeePayrollServiceTest.java
@@ -1,9 +1,8 @@
-
+//https://www.tutorialspoint.com/javatime/javatime_localdate_of1.htm
+import java.time.LocalDate;
 import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Test;
-
 
 
 public class EmployeePayrollServiceTest {
@@ -26,12 +25,20 @@ public class EmployeePayrollServiceTest {
 	    }
 
 	  @Test
-	    public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDB() {
+	    public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDB() throws EmployeePayrollException {
 		  employeePayrollService = new EmployeePayrollService();
 	      employeePayrollList = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
 	      employeePayrollService.updateEmployeeSalaryWithPreparedStatement("terisa", 20000.00);
 	      boolean result=employeePayrollService.checkEmployeePayrollInSyncWithDB("terisa");
 	      Assert.assertTrue(result);
 	  }
-	  
+	  @Test
+	    public void givenDateRange_WhenRetrievedEmployee_ShouldReturnEmpCount() throws EmployeePayrollException {
+	        employeePayrollService = new EmployeePayrollService();
+	        employeePayrollList = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+	        LocalDate startDate = LocalDate.of(2018, 01, 01);
+	        LocalDate endDate = LocalDate.now();
+	        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollDataForDateRange(startDate, endDate);
+	        Assert.assertEquals(3, employeePayrollList.size());
+	    }
 }
